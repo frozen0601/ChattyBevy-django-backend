84b5f2f7f8dd0211a02bfbb30765d01da9c87481
Andy Huang <52315929+frozen0601@users.noreply.github.com>
2023-02-14 15:40:11 -0500

    feat: the api_exception_handler now check if the request is legal. e.g. will now catch and retutn JSON error for a POST request to a GET API

--- bevyApi/messaging/api/urls.py
+++ bevyApi/messaging/api/urls.py
@@ -6,6 +6,7 @@ from rest_framework.routers import DefaultRouter
 from rest_framework.views import Response
 from .views import MessageViewset, RoomViewset
 from rest_framework.views import exception_handler
+from rest_framework import exceptions
 
 
 
@@ -22,30 +23,38 @@ urlpatterns = [
 
 def api_exception_handler(exc: Exception, context: dict[str, Any]) -> Response:
     """Custom API exception handler."""
-
-    # Call REST framework's default exception handler first,
-    # to get the standard error response.
-    response = exception_handler(exc, context)
-
-    if response is not None:
-        # Using the description's of the HTTPStatus class as error message.
-        http_code_to_message = {v.value: v.description for v in HTTPStatus}
-
-        error_payload = {
+    if isinstance(exc, exceptions.APIException):
+        response = Response(data={
             "error": {
-                "status_code": 0,
-                "message": "",
+                "status_code": exc.status_code,
+                "message": exc.default_detail,
                 "details": [],
             }
-        }
-        error = error_payload["error"]
-        status_code = response.status_code
+        }, status=exc.status_code)
+    else:
+        # Call REST framework's default exception handler first,
+        # to get the standard error response.
+        response = exception_handler(exc, context)
 
-        error["status_code"] = status_code
-        error["message"] = http_code_to_message[status_code]
-        error["details"] = response.data
-        response.data = error_payload
-    return response
+        if response is not None:
+            # Using the description's of the HTTPStatus class as error message.
+            http_code_to_message = {v.value: v.description for v in HTTPStatus}
+
+            error_payload = {
+                "error": {
+                    "status_code": 0,
+                    "message": "",
+                    "details": [],
+                }
+            }
+            error = error_payload["error"]
+            status_code = response.status_code
+
+            error["status_code"] = status_code
+            error["message"] = http_code_to_message[status_code]
+            error["details"] = response.data
+            response.data = error_payload
+        return response
 
 
 # # obsolete/learning paths
